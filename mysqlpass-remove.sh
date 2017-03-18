43e455292a62c9770149672d81da9cb67a45d7a0
root <root@centos6.(none)>
2017-03-18 16:59:14 +0400

    changes in addition scripts

--- mysqlpass-remove.sh
+++ mysqlpass-remove.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+#This script included in db-configure.sh script but also could be used separetly
+#to remove MySQL ROOT password
 service mysqld stop
 mysqld_safe --skip-grant-tables &
 printf "use mysql;\nupdate user set password=PASSWORD("") where User='root';\nflush privileges;\nquit" | mysql -uroot
