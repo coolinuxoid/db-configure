e0d749614927830a52c13809a4ba079f194f247e
root <root@centos6.(none)>
2017-03-18 16:59:07 +0400

    changes in addition scripts

--- pgsqlpass-remove.sh
+++ pgsqlpass-remove.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+#This script included in db-configure.sh script but also could be used separetly
+#to remove PostgreSQL ROOT password
 pghbaconf=`find / -name pg_hba.conf`
 pghbaconfremove='./pghbaconf-remove'
 pghbaconfset='./pghbaconf-set'
